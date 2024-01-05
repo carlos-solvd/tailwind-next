f77b342ac102c98023802560a82d31e76d1f3300
carlos-solvd <cealvesdegodoi@solvd.com>
2024-01-05 15:34:44 -0300

    feat: created a tab menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
   return (
-    <h1 className="dark:bg--slate-980 bg-slate-50 p-8 text-slate-900">
-      Hello World
-    </h1>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
   )
 }
